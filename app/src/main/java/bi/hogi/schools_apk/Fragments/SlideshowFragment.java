95c4cdda45db8395ad8c23d55edf90f04f4f54e9
GBstallman <nduwimanaguymich@gmail.com>
2021-06-22 14:32:25 -0700

    created navigationDrawer activity with fragments

---
+++ app/src/main/java/bi/hogi/schools_apk/Fragments/SlideshowFragment.java
@@ -0,0 +1,23 @@
+package bi.hogi.schools_apk.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import androidx.fragment.app.Fragment;
+
+
+import bi.hogi.schools_apk.R;
+
+public class SlideshowFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_slideshow, container, false);
+        return view;
+    }
+}
